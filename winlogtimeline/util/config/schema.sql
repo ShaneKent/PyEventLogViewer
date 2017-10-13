42611e8eeb6d5ece9442b9f2ddadfd211b7063e4
Carter Van Deuren <carter.van.deuren@gmail.com>
2017-10-13 10:47:58 -0700

    Implement the project class

---
+++ winlogtimeline/util/config/schema.sql
@@ -0,0 +1,29 @@
+/*
+   Each row in this table contains the relevant metadata for a log entry
+ */
+CREATE TABLE IF NOT EXISTS logs (
+  timestamp_utc DATETIME NOT NULL,
+  timestamp_local DATETIME,
+  timestamp_tz INTEGER NOT NULL,
+  event_id INTEGER NOT NULL,
+  description TEXT,
+  details TEXT,
+  event_source TEXT NOT NULL,
+  event_log TEXT NOT NULL,
+  session_id INTEGER NOT NULL,
+  account TEXT NOT NULL,
+  computer_name TEXT NOT NULL,
+  record_number INTEGER NOT NULL,
+  recovered BOOLEAN NOT NULL,
+  source_file_hash TEXT NOT NULL,
+  FOREIGN KEY(source_file_hash) REFERENCES source_files(hash)
+);
+/*
+    This table contains a row for each file which has been parsed for event logs.
+ */
+CREATE TABLE IF NOT EXISTS source_files (
+  file_name TEXT NOT NULL,
+  hash TEXT NOT NULL UNIQUE,
+  import_timestamp DATETIME NOT NULL,
+  computer_name TEXT
+);
