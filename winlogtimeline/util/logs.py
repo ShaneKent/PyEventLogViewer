088b3a66031d64991ad9a1ca501ef1d49afaa1df
zmonroe <zjmonroe23@gmail.com>
2017-10-18 11:17:20 -0700

    Record Object init

--- winlogtimeline/util/logs.py
+++ winlogtimeline/util/logs.py
@@ -1,3 +1,39 @@
+class Record:
+    def __init__(self, **kwargs):
+        self.timestamp_utc = kwargs['timestamp_utc']
+        self.event_id = kwargs['event_id']
+        self.description = kwargs['description']
+        self.details = kwargs['details']
+        self.event_source = kwargs['event_source']
+        self.event_log = kwargs['event_log']
+        self.session_id = kwargs['session_id']
+        self.account = kwargs['account']
+        self.computer_name = kwargs['computer_name']
+        self.record_number = kwargs['record_number']
+        self.recovered = kwargs['recovered']
+        self.source_file_hash = kwargs['source_file_hash']
+        self.record_hash = self.__hash__()
+
+
+    # get unique id function that returns unique id for each log
+    # __hash__ function (override)
+    # timestamp_utc, event_id, description, computer name, event source, event log
+    def __key__(self):
+        key_string = ""
+        key_string += self.timestamp_utc
+        key_string += self.event_id
+        key_string += self.description
+        key_string += self.computer_name
+        key_string += self.event_source
+        key_string += self.event_log
+        return key_string
+
+
+    def __hash__(self):
+        return hash(self.__key__)
+
+
+
 def parse_record(record, machine):
     """
     Takes a record as returned by libevtx-python and returns a dictionary representing the relevant log fields.
