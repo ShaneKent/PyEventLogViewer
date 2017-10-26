0af9da2d3686dff1f5f32631060b5ce3b6876d61
Carter Van Deuren <carter.van.deuren@gmail.com>
2017-10-25 18:49:46 -0700

    Refactor UI and add ttk overrides

--- winlogtimeline/ui/ui.py
+++ winlogtimeline/ui/ui.py
@@ -1,6 +1,6 @@
-import tkinter.filedialog as filedialog
-import tkinter.messagebox as messagebox
-import tkinter as tk
+from tkinter import *
+from tkinter import messagebox, filedialog
+from tkinter.ttk import *
 
 from threading import Thread
 from winlogtimeline import util
@@ -11,34 +11,34 @@ import os
 current_project = None
 
 
-class GUI(tk.Tk):
+class GUI(Tk):
     def __init__(self, *args, **kwargs):
-        tk.Tk.__init__(self, *args, **kwargs)
+        super().__init__(*args, **kwargs)
 
         self.winfo_toplevel().title("PyEventLogViewer")
         self.minsize(width=800, height=600)
 
-        self.menubar = Menubar(self)
-        self.statusbar = StatusBar(self)
+        self.menu_bar = MenuBar(self, tearoff=False)
+        self.status_bar = StatusBar(self)
         self.toolbar = Toolbar(self)
-        self.querybar = QueryBar(self)
-        self.eventsection = EventSection(self)
+        self.query_bar = QueryBar(self)
+        self.event_section = EventSection(self)
 
-        self.__disable__(*args, **kwargs)
+        self.__disable__()
         self.protocol("WM_DELETE_WINDOW", self.__destroy__)
 
-    def __disable__(self, *args, **kwargs):
-        self.toolbar.__disable__(self, *args, **kwargs)
-        self.querybar.__disable__(self, *args, **kwargs)
+    def __disable__(self):
+        self.toolbar.__disable__()
+        self.query_bar.__disable__()
 
-    def __enable__(self, *args, **kwargs):
-        self.toolbar.__enable__(self, *args, **kwargs)
-        self.querybar.__enable__(self, *args, **kwargs)
+    def __enable__(self):
+        self.toolbar.__enable__()
+        self.query_bar.__enable__()
 
     def __destroy__(self, *args, **kwargs):
         global current_project
 
-        if (current_project != None):
+        if current_project is not None:
             answer = messagebox.askyesno("Save Before Close", "Would you like to save the currently opened "
                                                               "project before closing it?")
 
@@ -49,109 +49,118 @@ class GUI(tk.Tk):
         self.destroy()
 
 
-class EventSection(tk.Canvas):
-    def __init__(self, parent, *args, **kwargs):
-        self.frame = tk.Frame(parent, *args, **kwargs)
-        self.frame.pack(fill=tk.BOTH)
+class EventSection(Frame):
+    def __init__(self, parent, **kwargs):
+        super().__init__(parent, **kwargs)
+        self.pack(fill=BOTH)
 
 
-class QueryBar(tk.Frame):
-    def __init__(self, parent, *args, **kwargs):
-        self.querybar = tk.Frame(parent, *args, **kwargs)
-        self.querybar.pack(side=tk.TOP, fill=tk.X)
+class QueryBar(Frame):
+    def __init__(self, parent, **kwargs):
+        super().__init__(parent, **kwargs)
+        self.pack(side=TOP, fill=X)
 
-        self.label = tk.Label(self.querybar, text="Show Events:", anchor=tk.W, *args, **kwargs)
-        self.label.pack(side=tk.LEFT)
+        self.label = Label(self, text="Show Events:", anchor=W, **kwargs)
+        self.label.pack(side=LEFT)
 
-        self.variable = tk.StringVar(self.querybar)
+        self.variable = StringVar(self)
         self.variable.set("All")
 
-        self.dropDown = tk.OptionMenu(self.querybar, self.variable, "System Startup", "System Shutdown", "Time Change",
-                                      "All")
-        self.dropDown.config(width="15")
-        self.dropDown.pack(side=tk.LEFT)
+        self.drop_down = OptionMenu(self, self.variable, "System Startup", "System Shutdown", "Time Change",
+                                   "All")
+        self.drop_down.config(width="15")
+        self.drop_down.pack(side=LEFT)
 
         # -- REMOVE WHEN NOT NEEDED. Only for prototyping reasons.
-        self.button = tk.Button(self.querybar, text="Query", command=lambda: parent.statusbar.status.config(
+        self.button = Button(self, text="Query", command=lambda: parent.status_bar.status.config(
             text="Queried for " + self.variable.get() + " events."))
-        self.button.pack(side=tk.LEFT)
+        self.button.pack(side=LEFT)
         # --
 
-    def __disable__(self, parent, *args, **kwargs):
-        self.button.config(state=tk.DISABLED)
-        self.dropDown.config(state=tk.DISABLED)
+    def __disable__(self):
+        self.button.config(state=DISABLED)
+        self.drop_down.config(state=DISABLED)
 
-    def __enable__(self, parent, *args, **kwargs):
-        self.button.config(state=tk.NORMAL)
-        self.dropDown.config(state=tk.NORMAL)
+    def __enable__(self):
+        self.button.config(state=NORMAL)
+        self.drop_down.config(state=NORMAL)
 
 
-class StatusBar(tk.Label):
+class StatusBar(Label):
     def __init__(self, parent, *args, **kwargs):
-        self.status = tk.Label(parent, text="Notice: Create a new project or open an existing project to get started.",
-                               bd=1, relief=tk.SUNKEN, anchor=tk.W, *args,
-                               **kwargs)
-        self.status.pack(side=tk.BOTTOM, fill=tk.X)
+        super().__init__(parent)
+        self.status = Label(parent, text="Notice: Create a new project or open an existing project to get started.",
+                            borderwidth=1, relief=SUNKEN, anchor=W, *args,
+                            **kwargs)
+        self.status.pack(side=BOTTOM, fill=X)
 
 
-class Toolbar(tk.Frame):
+class Toolbar(Frame):
     def __init__(self, parent, *args, **kwargs):
-        self.toolbar = tk.Frame(parent, bd=1, relief=tk.SUNKEN, *args, **kwargs)
+        super().__init__(parent, borderwidth=1, relief=SUNKEN, **kwargs)
 
-        self.importPhoto = tk.PhotoImage(file=util.data.get_package_data_path(__file__, 'icons', 'import.gif'))
-        self.formatPhoto = tk.PhotoImage(file=util.data.get_package_data_path(__file__, 'icons', 'format.gif'))
+        self.import_photo = PhotoImage(file=util.data.get_package_data_path(__file__, 'icons', 'import.gif'))
+        self.format_photo = PhotoImage(file=util.data.get_package_data_path(__file__, 'icons', 'format.gif'))
 
-        self.importButton = tk.Button(self.toolbar, image=self.importPhoto, width="20", height="20",
-                                      command=lambda: self.import_function(parent))
-        self.formatButton = tk.Button(self.toolbar, image=self.formatPhoto, width="20", height="20",
-                                      command=lambda: self.format_function(parent))
+        self.import_button = Button(self, image=self.import_photo, width="20",
+                                    command=lambda: self.import_function())
+        self.format_button = Button(self, image=self.format_photo, width="20",
+                                    command=lambda: self.format_function())
 
-        self.importButton.pack()
-        self.formatButton.pack()
+        self.import_button.pack()
+        self.format_button.pack()
 
-        self.toolbar.pack(side=tk.LEFT, fill=tk.Y)
+        self.pack(side=LEFT, fill=Y)
 
-    def __disable__(self, parent, *args, **kwargs):
-        self.importButton.config(state=tk.DISABLED)
-        self.formatButton.config(state=tk.DISABLED)
+    def __disable__(self):
+        self.import_button.config(state=DISABLED)
+        self.format_button.config(state=DISABLED)
 
-    def __enable__(self, parent, *args, **kwargs):
-        self.importButton.config(state=tk.NORMAL)
-        self.formatButton.config(state=tk.NORMAL)
+    def __enable__(self):
+        self.import_button.config(state=NORMAL)
+        self.format_button.config(state=NORMAL)
 
-    def import_function(self, parent):
+    def import_function(self):
+        filename = filedialog.askopenfilename(title="Open a Project File",
+                                              filetypes=(("Windows Event Log File", "*.evtx"),))
+
+        if len(filename) == 0:
+            return
+
+        filename = os.path.abspath(filename)
+
         def callback():
-            parent.statusbar.status.config(text="Reading in the Event Log file named System.evtx")
-            finish = collector.import_log("../tests/System.evtx", "", "")
-            parent.statusbar.status.config(text=finish)
+            self.master.status_bar.status.config(text="Reading in the Event Log file named System.evtx")
+            finish = collector.import_log(filename, "", "")
+            self.master.status_bar.status.config(text=finish)
 
         t = Thread(target=callback)
         t.start()
 
         return
 
-    def format_function(self, parent):
-        parent.statusbar.status.config(text="'Format' button pressed.")
+    def format_function(self):
+        self.master.status_bar.status.config(text="'Format' button pressed.")
         return
 
 
-class Menubar(tk.Frame):
-    def __init__(self, parent, *args, **kwargs):
-        self.menu = tk.Menu(parent, *args, **kwargs)
-        parent.config(menu=self.menu)
+class MenuBar(Menu):
+    def __init__(self, parent, **kwargs):
+        super().__init__(parent, **kwargs)
+        parent.config(menu=self)
 
-        self.fileMenu = tk.Menu(self.menu, *args, **kwargs)
+        self.file_menu = Menu(self, **kwargs)
 
-        self.menu.add_cascade(label="File", menu=self.fileMenu)
+        self.add_cascade(label="File", menu=self.file_menu)
 
-        self.fileMenu.add_command(label="New Project", command=lambda: self.new_project_function(parent))
-        self.fileMenu.add_command(label="Open Project", command=lambda: self.open_project_function(parent))
-        # self.fileMenu.add_command(label="Save Project", command=lambda: self.saveProjectFunction(parent))
+        self.file_menu.add_command(label="New Project", command=lambda: self.new_project_function())
+        self.file_menu.add_command(label="Open Project", command=lambda: self.open_project_function())
+        # self.fileMenu.add_command(label="Save Project", command=lambda: self.saveProjectFunction())
 
-    def new_project_function(self, parent):
+    def new_project_function(self):
         global current_project
 
-        if (current_project != None):
+        if current_project is not None:
             answer = messagebox.askyesno("Save Before Close", "Would you like to save the currently opened "
                                                               "project before closing it?")
             if answer:
@@ -160,12 +169,12 @@ class Menubar(tk.Frame):
 
         project_path = os.path.join(util.data.get_appdir(), 'Projects', 'New Project')
         current_project = util.project.Project(project_path)
-        parent.statusbar.status.config(text="Project created at " + current_project.get_path())
-        parent.__enable__()
+        self.master.status_bar.status.config(text="Project created at " + current_project.get_path())
+        self.master.__enable__()
 
         return
 
-    def open_project_function(self, parent):
+    def open_project_function(self):
         global current_project
 
         if current_project is not None:
@@ -179,12 +188,15 @@ class Menubar(tk.Frame):
         projects_path = os.path.join(util.data.get_appdir(), 'Projects')
         filename = filedialog.askopenfilename(initialdir=projects_path, title="Open a Project File",
                                               filetypes=(("ELV Project File", "*.elv"),))
+        if len(filename) == 0:
+            return
+
         current_project = util.project.Project(filename)
-        parent.statusbar.status.config(text="Project opened at " + current_project.get_path())
-        parent.__enable__()
+        self.master.status_bar.status.config(text="Project opened at " + current_project.get_path())
+        self.master.__enable__()
 
         return
 
-    def save_project_function(self, parent):
-        parent.statusbar.status.config(text="'Save Project' chosen from menu bar.")
+    def save_project_function(self):
+        self.master.status_bar.status.config(text="'Save Project' chosen from menu bar.")
         return
