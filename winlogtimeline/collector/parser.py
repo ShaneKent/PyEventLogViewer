8cf776a524b90acf7f19c4269c559e5fff26f3d9
ShaneKent <sdkent@calpoly.edu>
2017-11-03 12:01:37 -0700

    Parse the events a bit more to remove the issue of wrong Event IDs being input.

--- winlogtimeline/collector/parser.py
+++ winlogtimeline/collector/parser.py
@@ -1,58 +1,58 @@
 from pprint import PrettyPrinter
 
 
-def parser(record, object):
-    # object["description"]
-    # object["details"]
-    # object["session_id"]
-    # object["account"]
+def parser(raw, record):
+    # record["description"]
+    # record["details"]
+    # record["session_id"]
+    # record["account"]
 
-    event_id = object["event_id"]
+    event_id = record["event_id"]
 
     if event_id == "1":
-        object = parse_id_1(record, object)  # System    -   Wake Up
+        record = parse_id_1(raw, record)  # System    -   Wake Up
     elif event_id == "12":
-        object = parse_id_12(record, object)  # System    -   System Start
+        record = parse_id_12(raw, record)  # System    -   System Start
     elif event_id == "13":
-        object = parse_id_13(record, object)  # System    -   System Shutdown
+        record = parse_id_13(raw, record)  # System    -   System Shutdown
     elif event_id == "41":
-        object = parse_id_41(record, object)  # System    -   Shutdown Error
+        record = parse_id_41(raw, record)  # System    -   Shutdown Error
     elif event_id == "42":
-        object = parse_id_42(record, object)  # System    -   Sleep Mode
+        record = parse_id_42(raw, record)  # System    -   Sleep Mode
     elif event_id == "104":
-        object = parse_id_104(record, object)  # System    -   Log Cleared
+        record = parse_id_104(raw, record)  # System    -   Log Cleared
     elif event_id == "1074":
-        object = parse_id_1074(record, object)  # System    -   Shutdown Initiated
+        record = parse_id_1074(raw, record)  # System    -   Shutdown Initiated
     elif event_id == "1102":
-        object = parse_id_1102(record, object)  # Security  -   Log Cleared
+        record = parse_id_1102(raw, record)  # Security  -   Log Cleared
     elif event_id == "4616":
-        object = parse_id_4616(record, object)  # System    -   Time Change
+        record = parse_id_4616(raw, record)  # System    -   Time Change
     elif event_id == "4624":
-        object = parse_id_4624(record, object)  # System    -   Logon Events
+        record = parse_id_4624(raw, record)  # System    -   Logon Events
     elif event_id == "4634":
-        object = parse_id_4634(record, object)  # Security  -   Logoff (Network Connection)
+        record = parse_id_4634(raw, record)  # Security  -   Logoff (Network Connection)
     elif event_id == "4647":
-        object = parse_id_4647(record, object)  # Security  -   User-Initiated Logoff
+        record = parse_id_4647(raw, record)  # Security  -   User-Initiated Logoff
     elif event_id == "4720":
-        object = parse_id_4720(record, object)  # Security  -   Account Created
+        record = parse_id_4720(raw, record)  # Security  -   Account Created
     elif event_id == "4722":
-        object = parse_id_4722(record, object)  # Security  -   Account Enabled
+        record = parse_id_4722(raw, record)  # Security  -   Account Enabled
     elif event_id == "4723":
-        object = parse_id_4723(record, object)  # Security  -   User Changed Password
+        record = parse_id_4723(raw, record)  # Security  -   User Changed Password
     elif event_id == "4724":
-        object = parse_id_4724(record, object)  # Security  -   Privileged User Reset Password
+        record = parse_id_4724(raw, record)  # Security  -   Privileged User Reset Password
     elif event_id == "4725":
-        object = parse_id_4725(record, object)  # Security  -   Account Disabled
+        record = parse_id_4725(raw, record)  # Security  -   Account Disabled
     elif event_id == "4726":
-        object = parse_id_4726(record, object)  # Security  -   Account Deleted
+        record = parse_id_4726(raw, record)  # Security  -   Account Deleted
     elif event_id == "6008":
-        object = parse_id_6008(record, object)  # System    -   Shutdown Error
+        record = parse_id_6008(raw, record)  # System    -   Shutdown Error
     elif event_id == "6013":
-        object = parse_id_6013(record, object)  # System    -   System Status
+        record = parse_id_6013(raw, record)  # System    -   System Status
     else:
-        object = None  # EventID isn't being tracked.
+        record = None  # EventID isn't being tracked.
 
-    return object
+    return record
 
     """
     session_id = None
@@ -68,101 +68,101 @@ def parser(record, object):
     """
 
 
-def parse_id_1(record, object):
-    object["description"] = "Wake Up"
-    return object
+def parse_id_1(raw, record):
+    record["description"] = "Wake Up"
+    return record
 
 
-def parse_id_12(record, object):
-    object["description"] = "System Start"
-    return object
+def parse_id_12(raw, record):
+    record["description"] = "System Start"
+    return record
 
 
-def parse_id_13(record, object):
-    object["description"] = "System Shutdown"
-    return object
+def parse_id_13(raw, record):
+    record["description"] = "System Shutdown"
+    return record
 
 
-def parse_id_41(record, object):
-    object["description"] = "Shutdown Error"
-    return object
+def parse_id_41(raw, record):
+    record["description"] = "Shutdown Error"
+    return record
 
 
-def parse_id_42(record, object):
-    object["description"] = "Sleep Mode"
-    return object
+def parse_id_42(raw, record):
+    record["description"] = "Sleep Mode"
+    return record
 
 
-def parse_id_104(record, object):
-    object["description"] = "Log Cleared"
-    return object
+def parse_id_104(raw, record):
+    record["description"] = "Log Cleared"
+    return record
 
 
-def parse_id_1074(record, object):
-    object["description"] = "Shutdown Initiated"
-    return object
+def parse_id_1074(raw, record):
+    record["description"] = "Shutdown Initiated"
+    return record
 
 
-def parse_id_1102(record, object):
-    object["description"] = "Log Cleared"
-    return object
+def parse_id_1102(raw, record):
+    record["description"] = "Log Cleared"
+    return record
 
 
-def parse_id_4616(record, object):
-    object["description"] = "Time Change"
-    return object
+def parse_id_4616(raw, record):
+    record["description"] = "Time Change"
+    return record
 
 
-def parse_id_4624(record, object):
-    object["description"] = "Logon Events"
-    return object
+def parse_id_4624(raw, record):
+    record["description"] = "Logon Events"
+    return record
 
 
-def parse_id_4634(record, object):
-    object["description"] = "Logoff (Network Connection)"
-    return object
+def parse_id_4634(raw, record):
+    record["description"] = "Logoff (Network Connection)"
+    return record
 
 
-def parse_id_4647(record, object):
-    object["description"] = "User-Initiated Logoff"
-    return object
+def parse_id_4647(raw, record):
+    record["description"] = "User-Initiated Logoff"
+    return record
 
 
-def parse_id_4720(record, object):
-    object["description"] = "Account Created"
-    return object
+def parse_id_4720(raw, record):
+    record["description"] = "Account Created"
+    return record
 
 
-def parse_id_4722(record, object):
-    object["description"] = "Account Enabled"
-    return object
+def parse_id_4722(raw, record):
+    record["description"] = "Account Enabled"
+    return record
 
 
-def parse_id_4723(record, object):
-    object["description"] = "User Changed Password"
-    return object
+def parse_id_4723(raw, record):
+    record["description"] = "User Changed Password"
+    return record
 
 
-def parse_id_4724(record, object):
-    object["description"] = "Privileged User Reset Password"
-    return object
+def parse_id_4724(raw, record):
+    record["description"] = "Privileged User Reset Password"
+    return record
 
 
-def parse_id_4725(record, object):
-    object["description"] = "Account Disabled"
-    return object
+def parse_id_4725(raw, record):
+    record["description"] = "Account Disabled"
+    return record
 
 
-def parse_id_4726(record, object):
-    object["description"] = "Account Deleted"
-    return object
+def parse_id_4726(raw, record):
+    record["description"] = "Account Deleted"
+    return record
 
 
-def parse_id_6008(record, object):
-    object["description"] = "Shutdown Error"
-    return object
+def parse_id_6008(raw, record):
+    record["description"] = "Shutdown Error"
+    return record
 
 
-def parse_id_6013(record, object):
-    object["description"] = "System Status"
-    return object
+def parse_id_6013(raw, record):
+    record["description"] = "System Status"
+    return record
